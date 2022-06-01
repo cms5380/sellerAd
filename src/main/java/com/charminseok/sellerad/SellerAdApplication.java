28237bf871ed1c5dac12941534f3672b6e7559e9
cms5380 <cms1297@gmail.com>
2022-06-01 10:08:21 +0900

    spring boot init

---
+++ src/main/java/com/charminseok/sellerad/SellerAdApplication.java
@@ -0,0 +1,13 @@
+package com.charminseok.sellerad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellerAdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellerAdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/charminseok/sellerad/SellerAdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charminseok.sellerad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellerAdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
