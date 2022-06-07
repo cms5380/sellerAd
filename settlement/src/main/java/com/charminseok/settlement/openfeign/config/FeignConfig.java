eaeedc5cd392b825e7b29c7b286694e696d7c1d3
cms5380 <cms1297@gmail.com>
2022-06-08 03:14:54 +0900

    [정산] config

---
+++ settlement/src/main/java/com/charminseok/settlement/openfeign/config/FeignConfig.java
@@ -0,0 +1,20 @@
+package com.charminseok.settlement.openfeign.config;
+
+
+import com.charminseok.settlement.openfeign.error.FeignErrorDecoder;
+import feign.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfig {
+    @Bean
+    public Logger.Level feignLoggerLevel(){
+        return Logger.Level.FULL;
+    }
+
+    @Bean
+    public FeignErrorDecoder getFeignErrorDecoder(){
+        return new FeignErrorDecoder();
+    }
+}
