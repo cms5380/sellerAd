97eb669cf5fe2246cb74ed537605ed5402eb554f
cms5380 <cms1297@gmail.com>
2022-06-08 03:14:54 +0900

    [정산] config

---
+++ settlement/src/main/java/com/charminseok/settlement/service/SettlementService.java
@@ -0,0 +1,8 @@
+package com.charminseok.settlement.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SettlementService {
+
+}
--- settlement/src/test/java/com/charminseok/settlement/SettlementApplicationTests.java
+++ settlement/src/test/java/com/charminseok/settlement/SettlementApplicationTests.java
@@ -8,6 +8,7 @@ class SettlementApplicationTests {
 
     @Test
     void contextLoads() {
+
     }
 
 }
