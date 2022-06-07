6de01e9f3a07f96dfa4c3b024a73e430025b1f1e
cms5380 <cms1297@gmail.com>
2022-06-08 02:43:37 +0900

    [업체/계약] OPEN FEIGN 상품 API 수정

--- company/src/main/java/com/charminseok/company/openfeign/client/ProductService.java
+++ company/src/main/java/com/charminseok/company/openfeign/client/ProductService.java
@@ -3,10 +3,10 @@ package com.charminseok.company.openfeign.client;
 import com.charminseok.company.dto.ResponseProduct;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "product")
 public interface ProductService {
-    @GetMapping("/product/{companyName}")
-    ResponseProduct getProductByCompanyName(@PathVariable String companyName);
+    @GetMapping("/product")
+    ResponseProduct getProductByCompanyName(@RequestParam("company-name") String companyName);
 }
